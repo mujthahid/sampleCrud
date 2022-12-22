fdc2af58d081036431455cb273330e14b288ab7b
Mujthahid <mujthahidk@gmail.com>
2022-12-22 13:26:03 +0530

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const db = require('./database')
 const path = require('path')
 
 const hbs = require('hbs')
 require('dotenv').config()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.set('Views',path.join(__dirname))
